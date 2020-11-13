b783bf3cf61df7e770dd2f51b34b6d24c26a23f1
sukru <sukrugundogdudatascience@gmail.com>
2020-11-13 18:17:27 +0300

    fix bug and add movie db state

--- src/components/CardList/index.js
+++ src/components/CardList/index.js
@@ -1,13 +1,15 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import {Card} from "../Card"
 import axios from "axios";
 
 //8aad6bf4ccd0b4b0eee8d8deb8167978
 const apiKey = "8aad6bf4ccd0b4b0eee8d8deb8167978"; //temporary
 //https://api.themoviedb.org/3/search/movie?
-const baseUrl = "https://api.themoviedb.org/3/movie/";
+const baseUrl = "https://api.themoviedb.org/3/search/movie";
 
 export const CardList = () => {
+    //useState
+    const [movieData, setMovieData] = useState(null);
 
     //useEffect - Axios API call
     useEffect(() =>{
@@ -15,13 +17,14 @@ export const CardList = () => {
             params: {
                 api_key: apiKey,   //api_key=<<api_key>>&
                 page:1,             //page=1&
-                query:"Star Wars"   //TODO: from input query=test
+                query:"Star Wars"   
             }
         })
-        .then((res) => console.log(res));
-    },[]);    
-
-    //useState
+        .then((res) => setMovieData(res.data))
+        .catch((err) => console.log(err))
+    },[]);
+    
+    console.log(movieData)
     // data mapping    
     return (
         <Card/>
