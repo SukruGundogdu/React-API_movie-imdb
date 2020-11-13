a595e7fd87d0e8ea6b8558971c226f28210dcb56
sukru <sukrugundogdudatascience@gmail.com>
2020-11-13 16:39:50 +0300

    created card

---
+++ src/components/Card/index.js
@@ -0,0 +1,10 @@
+import {StyledCardWrapper, StyledText} from "./Card.style"
+
+
+export const Card = () => {
+    return (
+        <StyledCardWrapper>
+            <StyledText>CARD</StyledText>
+        </StyledCardWrapper>
+    )
+}
