8e220ff283571eb1aff5eb41a83cefe8c861c516
sukru <sukrugundogdudatascience@gmail.com>
2020-11-14 19:00:03 +0300

    Add MovieList & Searchbox

--- src/components/CardList/index.js
+++ src/components/CardList/index.js
@@ -1,37 +1,21 @@
-import React, {useEffect, useState} from "react";
-import {Card} from "../Card"
-import axios from "axios";
+import {Card} from "../Card";
+import {StyledCardList} from "./CardList.style"
 
-//8aad6bf4ccd0b4b0eee8d8deb8167978
-const apiKey = "8aad6bf4ccd0b4b0eee8d8deb8167978"; //temporary
-//https://api.themoviedb.org/3/search/movie?
-const baseUrl = "https://api.themoviedb.org/3/search/movie";
 
-export const CardList = () => {
-    //useState
-    const [movieData, setMovieData] = useState(null);
 
-    //useEffect - Axios API call
-    useEffect(() =>{
-        axios.get(baseUrl, {
-            params: {
-                api_key: apiKey,   //api_key=<<api_key>>&
-                page:1,             //page=1&
-                query:"Star Wars"   
-            }
-        })
-        .then((res) => setMovieData(res.data.results))
-        .catch((err) => console.log(err))
-    },[]);
-    
-       
+
+export const CardList = ({movieList, baseImageUrl}) => {
+
     return (
-        // data mapping 
-        <>
-        { movieData && movieData.map(movie => {
-            return <Card title={movie.title}/>
-        })
-            }
-        </>
+        <StyledCardList>
+           {movieList?.map(
+               (movie, index) => (
+               <Card 
+                key={index} 
+                title={movie.title}
+                imgSrc={baseImageUrl + movie.poster_path}
+                />)
+            )}
+        </StyledCardList>
     )
 }
