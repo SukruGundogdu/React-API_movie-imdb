585db66ce5692a7e7dd5106fad711bb393e307fb
sukru <sukrugundogdudatascience@gmail.com>
2020-11-14 20:26:48 +0300

    Add SearchBox Button Click

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,10 +1,8 @@
 import {StyledCardWrapper, StyledText, StyledMovieImage} from "./Card.style"
-
-
 export const Card = ({title, imgSrc}) => {
     return (
         <StyledCardWrapper>
-            <StyledMovieImage src={imgSrc}/>
+            <StyledMovieImage src={imgSrc} alt={"Movie Poster"}/>
             <StyledText>{title}</StyledText>
         </StyledCardWrapper>
     )
