9a3038701acd6b20baf035bccb8f79d816af2569
sukru <sukrugundogdudatascience@gmail.com>
2020-11-15 19:09:56 +0300

    add useRef and useContex

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, createContext } from "react";
 import axios from "axios";
+
 import './App.css';
 import {SearchBox} from "./components/SearchBox";
 import {CardList} from "./components/CardList";
 const apiKey = "7fc8d00ab1302c6aa7da237d2810116e"; //temporary
 const baseUrl = "https://api.themoviedb.org/3/search/movie";
 const baseImageUrl = "https://image.tmdb.org/t/p/w500";
+
+export const MovieContext  = createContext();
+
+
 function App() {
 
   const [movieList, setMovieList] = useState([]);
-  const [searchKeyword, setSearchKeyword] = useState("")
-
+  const [searchKeyword, setSearchKeyword] = useState("Matrix")
   useEffect(()=>{
       axios.get(
           baseUrl, {params: {
@@ -26,9 +30,12 @@ function App() {
 
   return (
     <div className="App">
-      <SearchBox setSearchKeyword={setSearchKeyword}/>
-      <CardList movieList={movieList} baseImageUrl={baseImageUrl} />
+      <MovieContext.Provider value={{ movieList, baseImageUrl, setSearchKeyword }}>
+          <SearchBox />
+          <CardList />
+      </MovieContext.Provider>
     </div>
   );
 }
+
 export default App;
--- src/components/SearchBox/index.js
+++ src/components/SearchBox/index.js
@@ -1,14 +1,15 @@
-import {useState} from 'react';
-import {StyledSearchBox, StyledSearchInput, StyledSearchButton} from "./SearchBox.style"
+import {useRef, useContext} from 'react';
+import {MovieContext} from "../../App";
+import {StyledSearchBox, StyledSearchInput, StyledSearchButton} from "./SearchBox.style";
 
+export const SearchBox = () => {
+    const inputRef = useRef();
 
-export const SearchBox = ({setSearchKeyword}) => {
-    const [inputValue, setInputValue] = useState("");
-
+    const {setSearchKeyword} = useContext(MovieContext);
     return (
         <StyledSearchBox>
-           <StyledSearchInput isColored={false} onChange={(e)=>setInputValue(e.target.value)}/>
-           <StyledSearchButton onClick={()=>setSearchKeyword(inputValue)}>Search</StyledSearchButton> 
+           <StyledSearchInput ref={inputRef} isColored={false}/>
+           <StyledSearchButton onClick={()=>setSearchKeyword(inputRef?.current?.value)}>Search</StyledSearchButton> 
         </StyledSearchBox>
     )
 }
